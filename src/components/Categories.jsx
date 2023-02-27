fa035aad20ce5cb64ef1797ade2a40dfa6dab557
Arkadiy <frizzzbe@yandex.ru>
2023-02-27 16:15:52 +0300

    Dynamic categories render

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,49 @@
+import React from "react";
+
 function Categories() {
+  const [activeIndx, setActive] = React.useState(0);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (i) => {
+    setActive(i);
+  };
+
+  const isActive = (i) => {
+    if (activeIndx == i) {
+      return "active";
+    } else {
+      return "";
+    }
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
+        {categories.map((el, idx) => {
+          return (
+            <li
+              onClick={() => onClickCategory(idx)}
+              className={ activeIndx === idx ? "active" : ''  }
+              // className={() => isActive(idx)}
+              key={el + "" + Math.random()}
+            >
+              {el}
+            </li>
+          );
+        })}
+        {/* <li className="active">Все</li>
         <li>Мясные</li>
         <li>Вегетарианская</li>
         <li>Гриль</li>
         <li>Острые</li>
-        <li>Закрытые</li>
+        <li>Закрытые</li> */}
       </ul>
     </div>
   );

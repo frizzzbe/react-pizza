0583b8f2dc024aa8be113ddae5c68e078cfedfe1
Arkadiy <frizzzbe@yandex.ru>
2023-02-28 17:18:34 +0300

    get pizzas JSON from fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./scss/app.scss";
+import React from 'react'
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizzas.json";
+// import pizzas from "./assets/pizzas.json";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(()=>{
+    fetch('https://63fe042bcd13ced3d7c47f84.mockapi.io/items')
+    .then((res)=>{return res.json()})
+    .then(json => {setPizzas(json)});
+  }, []);
+
+  // const getItems = () => {
+  // }
+
   return (
     <div className="wrapper">
       <Header />

68696fd9bb99bdd05be7a0c754389f8e5638c79c
Arkadiy <frizzzbe@yandex.ru>
2023-03-02 02:37:05 +0300

    JSON links changed to local/public

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -9,7 +9,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
 
   return (
     <div className="pizza-block">
-      <img className="pizza-block__image" src={imageUrl} alt={title} />
+      <img className="pizza-block__image" src={'/pizzasImg/' + imageUrl} alt={title} />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>

99b8dc04e25871ee177363a462a5902c1ae9847a
Arkadiy <frizzzbe@yandex.ru>
2023-03-02 23:46:09 +0300

    JSON links changed to local/public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import logoSvg from '../assets/img/pizza-logo.svg'
 import { Link } from "react-router-dom";
 
+import Search from "./Search";
+
 function Header() {
   return (
     <div className="header">
@@ -12,6 +14,7 @@ function Header() {
             <p>самая вкусная пицца во вселенной</p>
           </div>
         </Link>
+        <Search/>
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>

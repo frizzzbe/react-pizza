8a2be0c2fbb290abb01bbd2de20404b5e1bb9400
Arkadiy <frizzzbe@yandex.ru>
2023-03-04 21:33:05 +0300

    redux prettify

--- src/redux/Slices/filterSlice.js
+++ src/redux/Slices/filterSlice.js
@@ -1,29 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   count: 0, // начальное состояние = 0
-}
+};
 
 export const filterSlice = createSlice({
-  name: 'filter',
+  name: "filter",
   initialState,
   reducers: {
     increment: (state) => {
-      state.count += 1
+      state.count += 1;
     },
     increment15: (state) => {
-      state.count += 15
+      state.count += 15;
     },
     decrement: (state) => {
-      state.count -= 1
+      state.count -= 1;
     },
     incrementByAmount: (state, action) => {
-      state.count += action.payload
+      state.count += action.payload;
     },
   },
-})
+});
 
-export const { increment, decrement, incrementByAmount } = filterSlice.actions
+export const { increment, decrement, incrementByAmount } = filterSlice.actions;
 //  filterSlice.actions вернет все методы (действия) объекта filterSlice
-export default filterSlice.reducer
+export default filterSlice.reducer;
 // отвечает за изменения state у reducer

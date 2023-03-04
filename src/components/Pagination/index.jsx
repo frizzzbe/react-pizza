d78f13349c24d0ff99c1cfe58ee2588be5264d47
Arkadiy <frizzzbe@yandex.ru>
2023-03-04 18:09:02 +0300

    created redux and remove errors class

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -8,8 +8,8 @@ function Pagination({setCurrentPage}) {
     <ReactPaginate
       className={styles.root}
       breakLabel="..."
-      previousLabel={<svg fill="none" height="24" stroke-width="1.5" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M15 6L9 12L15 18" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/></svg>}
-      nextLabel={<svg fill="none" height="24" stroke-width="1.5" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M9 6L15 12L9 18" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/></svg>}
+      previousLabel={<svg fill="none" height="24" strokeWidth="1.5" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M15 6L9 12L15 18" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/></svg>}
+      nextLabel={<svg fill="none" height="24" strokeWidth="1.5" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M9 6L15 12L9 18" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/></svg>}
       onPageChange={(e) => setCurrentPage(e.selected + 1)}
       pageRangeDisplayed={5}
       pageCount={3}

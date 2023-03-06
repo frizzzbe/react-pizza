2046fdfd8d41c4f88bb2d04d6968c5ca8f178776
Arkadiy <frizzzbe@yandex.ru>
2023-03-06 15:12:33 +0300

    redux removed from filterslice

--- src/redux/Slices/filterSlice.js
+++ src/redux/Slices/filterSlice.js
@@ -1,32 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  count: 0, // начальное состояние = 0
-  // initial state может быть и простым значением не обязательно объектом
-  // const initialState = 0
-  // тогда обращение к нему можно сократить до 
-  // reducers: {
-  //   increment: (state) => state + 1,
-  // },
-};
+  categoryId: 0,
+  sort: 0,
+}
 
-export const filterSlice = createSlice({
-  name: "filter",
-  initialState,
-  reducers: {
-    increment: (state) => {
-      state.count += 1;
-    },
-    decrement: (state) => {
-      state.count -= 1;
-    },
-    // incrementByAmount: (state, action) => {
-    //   state.count += action.payload;
-    // },
-  },
+const filterSlice = createSlice({
+  
 });
 
-export const { increment, decrement, incrementByAmount } = filterSlice.actions;
-//  filterSlice.actions вернет все методы (действия) объекта filterSlice
-export default filterSlice.reducer;
-// отвечает за изменения state у reducer
+
+// const initialState = {
+//   count: 0, // начальное состояние = 0
+//   // initial state может быть и простым значением не обязательно объектом
+//   // const initialState = 0
+//   // тогда обращение к нему можно сократить до 
+//   // reducers: {
+//   //   increment: (state) => state + 1,
+//   // },
+// };
+
+// export const filterSlice = createSlice({
+//   name: "filter",
+//   initialState,
+//   reducers: {
+//     increment: (state) => {
+//       state.count += 1;
+//     },
+//     decrement: (state) => {
+//       state.count -= 1;
+//     },
+//     // incrementByAmount: (state, action) => {
+//     //   state.count += action.payload;
+//     // },
+//   },
+// });
+
+// export const { increment, decrement, incrementByAmount } = filterSlice.actions;
+// //  filterSlice.actions вернет все методы (действия) объекта filterSlice
+// export default filterSlice.reducer;
+// // отвечает за изменения state у reducer

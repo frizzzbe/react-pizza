3fe237b69b2c4cab126c6d46a93e7652766b2e0b
Arkadiy <frizzzbe@yandex.ru>
2023-03-07 21:54:20 +0300

    cart slice

---
+++ src/redux/Slices/cartSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  totalPrice: 0,
+  items: []
+}
+
+const filterSlice = createSlice({
+  name: 'filters',
+  initialState,
+  reducers: {
+    addProduct(state, action) {
+      state.items.push(action.payload)
+    }, 
+  }
+});
+
+export const { setCategoryId } = filterSlice.actions;
+export default filterSlice.reducer;

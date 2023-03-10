f2c8773adc5ba5bab845aa22a5b2d9078b42901c
Arkadiy <frizzzbe@yandex.ru>
2023-03-10 18:58:59 +0300

    outlet integration

--- src/App.js
+++ src/App.js
@@ -10,10 +10,18 @@ import Cart from "./pages/Cart";
 import FullPizza from "./pages/FullPizza";
 import NotFound from "./pages/NotFound";
 
+function Parent({children}) {
+  return <div>
+    <h1>Заголовок</h1>
+    {children}
+  </div>
+}
+
 function App() {
   return (
     <div className="wrapper">
       <Header />
+      <Parent>Content homie</Parent>
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
